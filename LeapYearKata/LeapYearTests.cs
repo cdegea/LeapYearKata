7b1eb30016c7d4ef2a0666b81113b6b21aa3920e
Cristian de Gea <cgea@aidacanarias.com>
2021-10-26 18:43:18 +0200

    Agregar archivos de proyecto.

---
+++ LeapYearKata/LeapYearTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LeapYearKata
+{
+    public class LeapYearTests
+    {
+        [Fact]
+        public void Is1904LeapYear()
+        {
+
+        }
+    }
+}
